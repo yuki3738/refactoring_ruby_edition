264c5e4eaefd709576f345fe7104bdad07112080
yuki3738 <y.minamiya@gmail.com>
2018-07-01 20:19:24 +0900

    1.7 料金コードによる条件分岐からポリモーフィズムへ
    frequent_renter_points メソッドを分割

--- lib/movie.rb
+++ lib/movie.rb
@@ -23,4 +23,8 @@ class Movie
       result += (days_rented - 3) * 1.5 if days_rented > 3
     end
   end
+
+  def frequent_renter_points(days_rented)
+    (price_code == Movie::NEW_RELEASE && days_rented > 1) ? 2 : 1
+  end
 end
